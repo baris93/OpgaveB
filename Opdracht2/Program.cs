88364bf14c175fe8db77353b46103fd0ed97d1f5
Baris <baryildiz93@gmail.com>
2017-11-19 03:08:38 +0100

    Test Commit

--- Opdracht2/Program.cs
+++ Opdracht2/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Opdracht2
 {
@@ -6,7 +8,71 @@ namespace Opdracht2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Program program = new Program();
+
+
+            //Commits
+            IList<Question> questions = new List<Question>()
+            {
+                new Question() { Category = "History", Difficulty = 2, Text = "In which year did the first World War Start ?", Answer = "1914"},
+                new Question() { Category = "Sport", Difficulty = 1, Text = "What does the NBA stand for ?", Answer = "National Basketball Association"},
+                new Question() { Category = "Science", Difficulty = 3, Text = "When was the Hubble Space Telescope launched into space ?", Answer = "1990"},
+                new Question() { Category = "Sport", Difficulty = 1, Text = "How are teams participate in the NBA? ", Answer = "30"},
+                new Question() { Category = "Tech", Difficulty = 2, Text = "Who is the founder of Tesla Motors? ", Answer = "Elon Musk"},
+                new Question() { Category = "Tech", Difficulty = 2, Text = "What does MB mean? ", Answer = "Megabyte"},
+                new Question() { Category = "Sport", Difficulty = 1, Text = "How many basketball teams are located in Los Angeles that play in the NBA? ", Answer = "2"},
+                new Question() { Category = "History", Difficulty = 2, Text = "In what year was the UN founded ? ", Answer = "1945"},
+                new Question() { Category = "Science", Difficulty = 3, Text = " ", Answer = "Elon Musk"}
+            };
+            
+            Console.WriteLine("Do you want to play based on difficulty or category ? \nfor Difficulty press 1, for Category press 2");
+            String inp = Console.ReadLine();
+
+            if (inp == "1")
+            {
+                var order = questions.OrderBy(x => x.Difficulty);
+
+                program.Play(order);
+            }
+            else if (inp == "2")
+            {
+                Console.WriteLine("Choose a following Category: History, Science, Sport, Tech");
+                String Choice = Console.ReadLine();
+                                
+                var category = questions.Where(x => x.Category == Choice);
+                program.Play(category);
+            }
+            else
+            {
+                Console.WriteLine("This choice is not possible");
+            }
+           
+            Console.ReadKey();
         }
+
+        public void Play(IEnumerable<Question> results)
+        {
+            int score = 0;
+            string failed = "";
+            foreach (var z in results)
+            {
+                z.Display();
+                String userAnswer = Console.ReadLine();
+                Console.WriteLine(z.checkAnswer(userAnswer));
+                if (z.checkAnswer(userAnswer) == true)
+                {
+                    score++;
+                }
+                else
+                {
+                    failed += "\n" + z.Text;
+                }
+            }
+                Console.WriteLine("Your Score is: " + score.ToString());
+                Console.WriteLine("\n");
+                Console.WriteLine("These were your wrong questions: " + failed);
+            
+        }
+
     }
 }
---
+++ Opdracht2/Question.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Opdracht2
+{
+    class Question
+    {
+        public int Difficulty { get; set; }
+        public string Category { get; set; }
+        public string Text { get; set; }
+        public string Answer { get; set; }
+
+        public Boolean checkAnswer(String repsonse)
+        {
+            if(String.Compare(repsonse, Answer) == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+           
+        }
+
+        public void Display()
+        {
+            Console.Write(Text);
+        }
+
+        
+
+    }
+}
